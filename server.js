76f79095c8717bf76915ac5d8dcd60ba2f48e55e
LokiGoiano <lorddavi19@gmail.com>
2024-12-26 14:00:53 -0300

    atualizações

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
+require('dotenv').config();
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use(cors());
 
-// Rotas aqui
+// Rota de registro
+app.post('/api/register', async (req, res) => {
+  const { firstName, lastName, cpf, phoneNumber, username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        cpf,
+        phoneNumber,
+        username,
+        password: hashedPassword
+      }
+    });
+    res.status(201).json({ message: 'User registered successfully', newUser });
+  } catch (error) {
+    res.status(500).json({ message: 'Error registering user', error: error.message });
+  }
+});
+
+// Rota de login
+app.post('/api/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (user && await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      res.json({ message: 'Login successful', token, userId: user.id });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error logging in', error: error.message });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
