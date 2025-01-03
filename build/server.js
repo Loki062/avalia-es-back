d672db6029d048ce062e7bf6bb4f3f818c4fd3ae
LokiGoiano <lorddavi19@gmail.com>
2025-01-03 11:15:01 -0300

    tsc corrigido

--- build/server.js
+++ build/server.js
@@ -1,62 +1,79 @@
-require('dotenv').config();
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
-
-const app = express();
-const prisma = new PrismaClient();
-
-app.use(express.json());
-app.use(cors());
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const prisma = new client_1.PrismaClient();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
-  res.send("Welcome to the API!");
+    res.send("Welcome to the API!");
 });
-
 // Rota de registro
 app.post('/api/register', async (req, res) => {
-  const { firstName, lastName, cpf, phoneNumber, username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  try {
-    const newUser = await prisma.user.create({
-      data: {
-        firstName,
-        lastName,
-        cpf,
-        phoneNumber,
-        username,
-        password: hashedPassword
-      }
-    });
-    res.status(201).json({ message: 'User registered successfully', newUser });
-  } catch (error) {
-    res.status(500).json({ message: 'Error registering user', error: error.message });
-  }
+    const { firstName, lastName, cpf, phoneNumber, username, password } = req.body;
+    const hashedPassword = await bcryptjs_1.default.hash(password, 10);
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                firstName,
+                lastName,
+                cpf,
+                phoneNumber,
+                username,
+                password: hashedPassword
+            }
+        });
+        res.status(201).json({ message: 'User registered successfully', newUser });
+    }
+    catch (error) { // Declara error como unknown
+        // Verifica se o erro é uma instância de Error
+        if (error instanceof Error) {
+            res.status(500).json({ message: 'Error registering user', error: error.message });
+        }
+        else {
+            // Se não for uma instância de Error, retorna uma mensagem genérica
+            res.status(500).json({ message: 'Error registering user', error: 'An unexpected error occurred' });
+        }
+    }
 });
-
 // Rota de login
 app.post('/api/login', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await prisma.user.findUnique({ where: { username } });
-    if (user && await bcrypt.compare(password, user.password)) {
-      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      res.json({ message: 'Login successful', token, userId: user.id });
-    } else {
-      res.status(401).json({ message: 'Invalid credentials' });
+    const { username, password } = req.body;
+    try {
+        const user = await prisma.user.findUnique({ where: { username } });
+        if (user && await bcryptjs_1.default.compare(password, user.password)) {
+            const secret = process.env.JWT_SECRET;
+            if (typeof secret !== 'string') {
+                throw new Error('JWT secret is undefined');
+            }
+            const token = jsonwebtoken_1.default.sign({ userId: user.id }, secret, { expiresIn: '1h' });
+            res.json({ message: 'Login successful', token, userId: user.id });
+        }
+        else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
     }
-  } catch (error) {
-    res.status(500).json({ message: 'Error logging in', error: error.message });
-  }
+    catch (error) { // Declara error como unknown
+        // Verifica se o erro é uma instância de Error
+        if (error instanceof Error) {
+            res.status(500).json({ message: 'Error registering user', error: error.message });
+        }
+        else {
+            // Se não for uma instância de Error, retorna uma mensagem genérica
+            res.status(500).json({ message: 'Error registering user', error: 'An unexpected error occurred' });
+        }
+    }
 });
-
-const port = 3333;
-
-const PORT = process.env.PORT || 3000;
-app.listen(process.env.PORT || port, () => {
-  console.log(`Server listening on port ${port}`);
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
